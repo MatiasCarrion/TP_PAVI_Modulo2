e296756395c36e3eb29b264b831e7bcd8842f0fe
Daniel Gauna <danicarpsv@gmail.com>
2021-09-14 20:17:54 -0300

    abm inicial

---
+++ TP_PAVI_Modulo2/TP_PAVI_Modulo2/Entidades/Curso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_PAVI_Modulo2.Entidades
+{
+    public class Curso
+    {
+        public int idCurso { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public DateTime fechaVigencia { get; set; }
+        public int idCategoria { get; set; }
+        public bool borrado { get; set; }
+    }
+}
--- TP_PAVI_Modulo2/TP_PAVI_Modulo2/Entidades/Usuario.cs
+++ TP_PAVI_Modulo2/TP_PAVI_Modulo2/Entidades/Usuario.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace TP_PAVI_Modulo2.Entidades
 {
-    class Usuario
+    public class Usuario
     {
+        public int idUsuario { get; set; }
+        public string nombreUsuario { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public Perfil perfil { get; set; }
+        public override string ToString()
+        {
+            return nombreUsuario;
+        }
     }
 }
